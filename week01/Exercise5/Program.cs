8339ac3ba85ff8c35e33d12d9bb878127df74e6b
madalynq <iwa.maddy@gmail.com>
2025-05-10 12:53:14 -0600

    w1 exercise5 functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMsg();
+        string userName = PromptUserName();
+        int favNum = PromptUserNumber();
+        int squared = SquaredNumber(favNum);
+
+        DisplayResult(userName, squared);
+    } 
+        
+    static void DisplayWelcomeMsg()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber() 
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    static int SquaredNumber(int favNum)
+    {
+        int square = favNum * favNum;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
