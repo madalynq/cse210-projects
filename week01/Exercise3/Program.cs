f629846c0b7b0f2bf4ee94a1f57305f890f54c33
madalynq <iwa.maddy@gmail.com>
2025-05-10 09:40:35 -0600

    w1 exercise3 loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   string tryAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 100);
+            
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNum)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > magicNum)
+                {
+                    Console.WriteLine("Lower");           
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess == magicNum)
+                {
+                    Console.WriteLine("You Guessed It!");
+                }
+
+            }
+            Console.Write("Do you want to try again?(yes/no): ");
+            tryAgain = Console.ReadLine().ToLower();
+        } while (tryAgain == "yes");
+        Console.WriteLine("Thank you for playing!");
     }
 }
