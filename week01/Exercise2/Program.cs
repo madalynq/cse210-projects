618ce2a390717607c81d40f0c981856f68148cf2
madalynq <iwa.maddy@gmail.com>
2025-05-10 08:29:03 -0600

    w1 exercise 2 if statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = " ";
+        string sign = " ";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        
+        }
+        else
+        {
+            letter = "F";
+        
+        }
+        int lastDigit = grade % 10;
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        if (letter == "A" && grade >= 93)
+        {
+            sign = " ";
+        }
+        Console.WriteLine($"{letter}{sign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
