0173b24a38c5ff15fd8e40153da646f990cb0129
madalynq <iwa.maddy@gmail.com>
2025-05-13 14:59:06 -0600

    W02 Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        // job1.Display();
+        // job2.Display();
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        resume1._job.Add(job1);
+        resume1._job.Add(job2);
+        
+        // Console.WriteLine(resume1._job[0]._jobTitle);
+
+        resume1.Display();
     }
 }
