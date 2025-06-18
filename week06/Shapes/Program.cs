516e2893f967c4838e29acdc0d397caca0450ff6
madalynq <iwa.maddy@gmail.com>
2025-06-17 18:39:23 -0600

    W06 Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4.0));
+        shapes.Add(new Rectangle("Blue", 5.0, 3.0));
+        shapes.Add(new Circle("Green", 2.5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
